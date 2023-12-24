0b4d61fe9e9b39303d88616c2b4f128ecde4f62a
Bhaskar Jyoti Pathak <125452211+Bhaskar-J-Pathak@users.noreply.github.com>
2023-12-24 22:47:23 +0530

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; Bhaskar J Pathak Dev 2023 Inc. All rights reserved
+                &copy; Bhaskar J Pathak
             </div>
         </div>
     </div>
