e258877d25c74ebf3e655c9ac0e44b0d85fe563f
Bhaskar Jyoti Pathak <125452211+Bhaskar-J-Pathak@users.noreply.github.com>
2024-01-03 14:29:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,43 @@
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import StarsCanvas from "@/components/main/StarBackground";
+// import Navbar from "@/components/main/Navbar";
+// import Footer from "@/components/main/Footer";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Arthur's Crib",
+//   description: "This is my portfolio",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+//       >
+//         <StarsCanvas />
+//         <Navbar />
+//         {children}
+//         <Footer />
+//       </body>
+//     </html>
+//   );
+// }
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
-import Footer from "@/components/main/Footer";
+//import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,8 +59,9 @@ export default function RootLayout({
         <StarsCanvas />
         <Navbar />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
 }
+
