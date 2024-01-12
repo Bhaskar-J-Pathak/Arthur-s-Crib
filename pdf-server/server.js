775b6f6732b4f765f645965c8fd8435db57dea9e
Bhaskar Jyoti Pathak <125452211+Bhaskar-J-Pathak@users.noreply.github.com>
2024-01-12 22:29:46 +0530

    Update server.js

--- pdf-server/server.js
+++ pdf-server/server.js
@@ -1,11 +1,30 @@
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// // Enable CORS for all routes
+// app.use(cors());
+
+// // Serve the PDF file
+// app.get('/pdf', (req, res) => {
+//   const pdfPath = path.join(__dirname, 'public', 'resume1.pdf');
+//   res.sendFile(pdfPath);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3001;
 
-// Enable CORS for all routes
 app.use(cors());
 
 // Serve the PDF file
@@ -14,6 +33,5 @@ app.get('/pdf', (req, res) => {
   res.sendFile(pdfPath);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
+
