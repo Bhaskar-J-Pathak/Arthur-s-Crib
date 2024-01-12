0dfed8f6fbd84da7228a445bc5618760a0c0d6e2
Bhaskar Jyoti Pathak <125452211+Bhaskar-J-Pathak@users.noreply.github.com>
2024-01-12 22:26:11 +0530

    Update server.js

--- pdf-server/server.js
+++ pdf-server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Enable CORS for all routes
 app.use(cors());
